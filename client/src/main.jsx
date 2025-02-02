3579dbe41233e69ca0456b55dded68589b41d04f
awhr95 <awhr95@gmail.com>
2025-02-02 14:58:27 +0000

    Added react router to client folder

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from "react-dom/client";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
