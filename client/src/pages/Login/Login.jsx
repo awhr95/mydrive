da9e5fe1bdb98c72bd7546415fd11c2610cfcd3f
awhr95 <awhr95@gmail.com>
2025-02-02 15:35:27 +0000

    Added bare bones sign up flow

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,7 +1,20 @@
 import "./Login.scss";
 
 const Login = () => {
-  return <h1>Login Page</h1>;
+  return (
+    <>
+      <h1>Login Page</h1>
+
+      <form className="login__form">
+        <label>Email</label>
+        <input type="text" />
+        <label>Password</label>
+        <input type="text" name="" id="" />
+
+        <button type="submit">Login/Sign up</button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
