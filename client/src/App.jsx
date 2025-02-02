f17ad1853c610b49129388ef31570f9692aa7d7f
awhr95 <awhr95@gmail.com>
2025-02-02 14:30:44 +0000

    Added login page scaffolding

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <>
       <h1>MyDrive</h1>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
---
+++ client/src/pages/Login/Login.jsx
@@ -0,0 +1,7 @@
+import "./Login.scss";
+
+const Login = () => {
+  return <h1>Login Page</h1>;
+};
+
+export default Login;
