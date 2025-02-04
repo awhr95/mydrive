e6c9e05811aa820e4ceba7c3ea7770daf3c8aa72
awhr95 <awhr95@gmail.com>
2025-02-04 19:27:24 +0000

    Added api user sign up post

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,24 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const USER_FILE = "users.json";
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/signup", (req, res) => {
+app.post("/users/login", (req, res) => {
   const { email, password } = req.body;
 
   const userData = { email, password };
-  fs.readFile("users.json", "utf8", (err, data) => {
+  fs.readFile(USER_FILE, "utf8", (err, data) => {
     if (err) return res.status(500).send("Error reading data");
     const users = data ? JSON.parse(data) : [];
     users.push(userData);
-    fs.writeFile("users.json", JSON.stringify(users, null, 2), (err) => {
+    fs.writeFile(USER_FILE, JSON.stringify(users, null, 2), (err) => {
       if (err) return res.status(500).send("Error saving data");
       res.status(200).send("User signed up");
     });
--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -2,26 +2,38 @@ import "./Login.scss";
 import axios from "axios";
 
 const Login = () => {
-  //   const handleSubmit = async (event) =>{
-  //     event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-  //     try {
-  //       const response = await axios.post(
-  //         'http://localhost:5173/'
-  //       )
-  //     }
+    const email = event.target.email.value;
+    const password = event.target.password.value;
 
-  // return repson;
-  //   }
+    if (!email || !password) {
+      console.log("Email and password are required.");
+      return;
+    }
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/users/login`,
+        {
+          email,
+          password,
+        }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <h1>Login Page</h1>
 
-      <form className="login__form">
+      <form className="login__form" onSubmit={handleSubmit}>
         <label>Email</label>
-        <input type="text" />
+        <input type="text" name="email" />
         <label>Password</label>
-        <input type="text" name="" id="" />
+        <input type="password" name="password" id="" />
 
         <button type="submit">Login/Sign up</button>
       </form>
