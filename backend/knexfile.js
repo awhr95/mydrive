651b05e1cf4329e100d7147727dc8ef47e352a5f
awhr95 <awhr95@gmail.com>
2025-02-10 12:56:21 +0000

    Added knex schema for user table creation in db

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,11 +1,33 @@
-require("dotenv").config();
+// Update with your config settings.
 
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 module.exports = {
-  client: "mysql2",
-  connection: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_LOCAL_DBNAME,
-    user: process.env.DB_LOCAL_USER,
-    password: process.env.DB_LOCAL_PASSWORD,
+  development: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_LOCAL_USER,
+      password: process.env.DB_LOCAL_PASSWORD,
+      database: process.env.DB_LOCAL_DBNAME,
+    },
+  },
+
+  production: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_LOCAL_USER,
+      password: process.env.DB_LOCAL_PASSWORD,
+      database: process.env.DB_LOCAL_DBNAME,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
   },
 };
